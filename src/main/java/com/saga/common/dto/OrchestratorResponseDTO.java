1b2bb2e91b34c7711ae7bdf9b0feb9d58793adf5
quocbao64 <quocbao642002@gmail.com>
2023-08-14 14:59:21 +0700

    remove serializable

--- src/main/java/com/saga/common/dto/OrchestratorResponseDTO.java
+++ src/main/java/com/saga/common/dto/OrchestratorResponseDTO.java
@@ -2,15 +2,10 @@ package com.saga.common.dto;
 
 import com.saga.common.enums.OrderStatus;
 import lombok.Data;
-
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.UUID;
 
 @Data
-public class OrchestratorResponseDTO implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
+public class OrchestratorResponseDTO {
     private Integer userID;
     private Integer productID;
     private UUID orderID;
